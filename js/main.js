af2bb6705e51abdf1e517e99d1401bdd05572e96
Luis Junco <38411763+ludovico-wd@users.noreply.github.com>
2022-09-15 17:12:46 +0200

    create and move obstacles

--- js/main.js
+++ js/main.js
@@ -2,10 +2,24 @@
 class Game {
     constructor(){
         this.player = null; //will store an instance of the class Player
+        this.obstacles = []; //will store instances of the class Obstacle
     }
     start(){
         this.player = new Player();
         this.attachEventListeners();
+        
+        //move obstacles
+        setInterval(() => {
+            this.obstacles.forEach( (obstacleInstance) => {
+                obstacleInstance.moveDown();
+            });
+        }, 60);
+
+        //create new obstacles
+        setInterval(() => {
+            const newObstacle = new Obstacle();
+            this.obstacles.push(newObstacle);
+        }, 3000);
     }
     attachEventListeners(){
         document.addEventListener("keydown", (event) => {
@@ -54,6 +68,37 @@ class Player {
     }
 }
 
+
+class Obstacle {
+    constructor(){
+        this.positionX = 50;
+        this.positionY = 90;
+        this.width = 10;
+        this.height = 10;
+        this.domElement = null;
+
+        this.createDomElement();
+    }
+    createDomElement(){
+        // create dom element
+        this.domElement = document.createElement('div');
+
+        // set id and css
+        this.domElement.className = "obstacle";
+        this.domElement.style.width = this.width + "vw";
+        this.domElement.style.height = this.height + "vh";
+        this.domElement.style.bottom = this.positionY + "vh";
+        this.domElement.style.left = this.positionX + "vw";
+
+        // append to the dom
+        const boardElm = document.getElementById("board");
+        boardElm.appendChild(this.domElement)
+    }
+    moveDown(){
+        this.positionY--;
+        this.domElement.style.bottom = this.positionY + "vh";
+    }
+}
 
 const game = new Game();
 game.start();
