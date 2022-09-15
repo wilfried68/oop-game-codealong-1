37c74d87ec537960359698eb46c7fd5ece7cf331
Luis Junco <38411763+ludovico-wd@users.noreply.github.com>
2022-09-15 15:12:42 +0200

    implement functionality to display and move player

--- js/main.js
+++ js/main.js
@@ -1,1 +1,61 @@
-console.log("my js file was loaded....")
+
+class Game {
+    constructor(){
+        this.player = null; //will store an instance of the class Player
+    }
+    start(){
+        this.player = new Player();
+        this.attachEventListeners();
+    }
+    attachEventListeners(){
+        document.addEventListener("keydown", (event) => {
+            if(event.key === "ArrowLeft"){
+                this.player.moveLeft();
+            }else if(event.key === "ArrowRight"){
+                this.player.moveRight();
+            }
+        });
+    }
+}
+
+
+class Player {
+    constructor(){
+        this.positionX = 50;
+        this.positionY = 0;
+        this.width = 20;
+        this.height = 10;
+        this.domElement = null;
+
+        this.createDomElement();
+    }
+    createDomElement(){
+        // create dom element
+        this.domElement = document.createElement('div');
+
+        // set id and css
+        this.domElement.id = "player";
+        this.domElement.style.width = this.width + "vw";
+        this.domElement.style.height = this.height + "vh";
+        this.domElement.style.bottom = this.positionY + "vh";
+        this.domElement.style.left = this.positionX + "vw";
+
+        // append to the dom
+        const boardElm = document.getElementById("board");
+        boardElm.appendChild(this.domElement)
+    }
+    moveLeft(){
+        this.positionX--;
+        this.domElement.style.left = this.positionX + "vw";
+    }
+    moveRight(){
+        this.positionX++;
+        this.domElement.style.left = this.positionX + "vw";
+    }
+}
+
+
+const game = new Game();
+game.start();
+
+
